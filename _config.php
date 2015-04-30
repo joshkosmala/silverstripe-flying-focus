b1be050bfaa86ed6552c218c93fc74e911f14a1e
Josh Kosmala <josh@novaweb.co.nz>
2015-04-30 18:44:14 +1200

    Check module dir exists

--- _config.php
+++ _config.php
@@ -1,3 +1,8 @@
 <?php
 
 define('MODULE_FLYINGFOCUS_DIR', basename(dirname(__FILE__)));
+/**
+ * Check if the module folder exists.
+ */
+if (basename(dirname(__FILE__)) != MODULE_FLYINGFOCUS_DIR) {
+    throw new Exception(MODULE_FLYINGFOCUS_DIR . ' should have the directory name flyingfocus in your website root');
