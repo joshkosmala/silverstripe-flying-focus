b7fdcba5d3d0b7bc090efe061b51f37a29455246
Josh Kosmala <josh@novaweb.co.nz>
2015-04-30 12:20:14 +1200

    Changing to onAfterInit

--- code/FlyingFocusExtension.php
+++ code/FlyingFocusExtension.php
@@ -2,7 +2,7 @@
 	
 class FlyingFocusExtension extends Extension {
 	
-	public function onBeforeInit() {
+	public function onAfterInit() {
 		// Include Flying Focus JavaScript TODO Change to dynamic URL segment
 		Requirements::javascript('flyingfocus/javascript/flying-focus.js');
 	} 
