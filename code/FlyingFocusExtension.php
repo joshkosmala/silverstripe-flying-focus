2dbf3efc92a713d9d4d123779b4937449db593e4
Namida Verasche <nami@northlandtechnology.nz>
2017-12-07 12:03:26 +1300

    Kinda plays nicely with SilverStripe 4 now. Except for the part where the Javascript file doesn't get automatically added to every page... which is kind of the whole point... (By manually modifying the templates to add it, it works fine, though. Just doesn't get auto-added.)

--- code/FlyingFocusExtension.php
+++ code/FlyingFocusExtension.php
@@ -1,10 +1,12 @@
 <?php
-	
-class FlyingFocusExtension extends Extension {
-	
-	public function onAfterInit() {
-		Requirements::javascript(MODULE_FLYINGFOCUS_DIR.'/javascript/flying-focus.js');
-	} 
-	
-}
 
+use SilverStripe\Core\Extension;
+use SilverStripe\View\Requirements;
+
+class FlyingFocusExtension extends Extension {
+
+	public function onAfterInit() {
+		Requirements::javascript('flyingfocus: /javascript/flying-focus.js');
+	}
+
+}
